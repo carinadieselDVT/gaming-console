6d2dc6e589b0bd05dcd6054f4dc3b5ecaffaca8e
Sashen <SGovindasamy@dvtsoftware.com>
2026-02-20 11:04:41 +0200

    Included actionTypes that will be used in tetris and cleaned up style files app and index.

--- src/components/layout/ConsoleAreaUI.tsx
+++ src/components/layout/ConsoleAreaUI.tsx
@@ -1,6 +1,23 @@
-import { Box, Button, ButtonGroup } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
-interface ConsoleAreaUIProps {}
+//list of actions that we'll use for the tetris game, we can use these for the actions to our game logic
+const ACTIONS = {
+  MOVE_LEFT: "MOVE_LEFT",
+  MOVE_RIGHT: "MOVE_RIGHT",
+  ROTATE: "ROTATE",
+  SOFT_DROP: "SOFT_DROP",
+  START: "START",
+  PAUSE: "PAUSE",
+} as const;
+
+//these actions can be only one of the values from the ACTIONS
+type ActionType = typeof ACTIONS[keyof typeof ACTIONS];
+
+
+interface ConsoleAreaUIProps {
+  onAction: (action: ActionType) => void;
+  gameState: "playing" | "paused" | "gameover" | "idle";
+}
 
 function ConsoleAreaUI({}: ConsoleAreaUIProps) {
   return <div>{/*  Add controls*/} Console Area</div>;
